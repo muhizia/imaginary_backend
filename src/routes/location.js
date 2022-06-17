f09da957d33d2a9fd0880749c5e36badfa882000
Aristide Muhizi <muhizia4@gmail.com>
2022-06-17 23:14:35 +0200

    adding routes for adding a location and updating

---
+++ src/routes/location.js
@@ -0,0 +1,71 @@
+/**
+ * This is the route file which contains get requests routes.
+ *
+ * @author Aristide Muhizi
+ *
+ * @email: muhizia4@gmail.com
+ *
+ *
+ */
+require('dotenv').config()
+const koaRouter = require('koa-router');
+const router = new koaRouter();
+const locationService = require('../services/location')
+const { validateEmail } = require('../util/validate');
+
+router.get('/', async ctx => {
+    ctx.status = 201;
+    ctx.body = { "status": "failed", "msg": "Enter a correct email" };
+});
+
+router.post('/add', async ctx => {
+    const { lat, long } = ctx.request.body;
+    var error = false;
+    console.log('---->', lat, ' ', long)
+    if (!lat) {
+        ctx.throw(400, 'Provide a valid latitude');
+    }
+    if (!long) {
+        ctx.throw(400, 'Provide a valid longitude');
+    }
+    
+    
+    const location = await index.addLocation(long, lat);
+    console.log(location)
+    if (location[0]) {
+        ctx.status = 201;
+        ctx.body = { "status": "success", "msg": "You have successfuly added a location" };
+    } else {
+        ctx.status = 400;
+        ctx.body = { "status": "fail", "msg": "failed to insert" };
+    }
+});
+
+router.put('/edit', async ctx => {
+    const { id, lat, long } = ctx.request.body;
+    var error = false;
+
+    if(!id) {
+        ctx.throw(400, 'Provide a valid ID');
+    }
+    if (!lat) {
+        ctx.throw(400, 'Provide a valid latitude');
+    }
+    if (!long) {
+        ctx.throw(400, 'Provide a valid longitude');
+    }
+    
+    
+    const location = await locationService.updateLocation(id, long, lat);
+    if (location[0]) {
+        ctx.status = 201;
+        ctx.body = { "status": "success", "msg": "You have successfuly updated a location" };
+    } else {
+        ctx.status = 400;
+        ctx.body = { "status": "fail", "msg": "failed to update check if the id exists" };
+    }
+
+    
+});
+
+module.exports = router.routes();
